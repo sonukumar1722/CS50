a92a8c6b648d3c81a53fa831881db61610652ee2
Sonu Kumar Shah <158548920+sonukumar1722@users.noreply.github.com>
2024-09-13 02:33:42 +0530

    Update models.py

--- CS50w/project4/network/models.py
+++ CS50w/project4/network/models.py
@@ -1,7 +1,5 @@
-from email.policy import default
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
 
 class User(AbstractUser):
     pass
@@ -9,16 +7,21 @@ class User(AbstractUser):
 class NewPost(models.Model):
     content = models.TextField(null=True)
     datetime = models.DateTimeField(auto_now_add=True)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE,related_name='creators')
-    likes = models.IntegerField(default = 0)
-    comment = models.CharField(max_length=200, blank = True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    likes = models.IntegerField(default=0)
+    comment = models.CharField(max_length=200, blank=True)
 
 class Follow(models.Model):
-    follower = models.ForeignKey(User, on_delete=models.CASCADE,related_name='followers')
-    following = models.ForeignKey(User, on_delete=models.CASCADE,related_name='followings')
-    followed = models.BooleanField(default = False)
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    
+    class Meta:
+        unique_together = ('follower', 'following')
 
 class Likes(models.Model):
-    liked = models.BooleanField(default = False)
-    liked_user = models.ForeignKey(User, on_delete = models.CASCADE, related_name='user_id')
-    liked_post = models.ForeignKey(NewPost, on_delete = models.CASCADE, related_name='post_id')
+    liked = models.BooleanField(default=False)
+    liked_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='liked_posts')
+    liked_post = models.ForeignKey(NewPost, on_delete=models.CASCADE, related_name='liked_by')
+
+    class Meta:
+        unique_together = ('liked_user', 'liked_post')
